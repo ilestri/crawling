433559bfc92935461bba2137fd548c797576e9a8
ZIBAGO <cownsgur01@gmail.com>
2024-12-13 10:50:56 +0900

    구글 추가

--- croling.py
+++ croling.py
@@ -26,6 +26,7 @@ options.add_argument('--ignore-ssl-errors')
 driver = webdriver.Chrome(service=service, options=options)
 wait = WebDriverWait(driver, 10)  # 10초 대기
 
+
 # 폴더 생성 함수
 def createFolder(name):
   if not os.path.exists(f'./{name}'):
@@ -34,12 +35,14 @@ def createFolder(name):
   else:
     print('이미 존재하는 폴더입니다.')
 
+
 # 인코딩 문제 해결을 위한 함수
 def save_base64_image(data, file_path):
   """Base64로 인코딩된 이미지 데이터를 파일로 저장합니다."""
   image_data = base64.b64decode(data)
   with open(file_path, 'wb') as f:
     f.write(image_data)
+
 
 # 파일명에서 사용할 수 없는 문자를 제거하는 함수
 def sanitize_filename(filename):
@@ -48,33 +51,82 @@ def sanitize_filename(filename):
     filename = filename.replace(char, "")
   return filename[:250]
 
+
+# 검색 엔진 선택
+print("검색 엔진 선택:")
+print("1. 네이버")
+print("2. 구글")
+engine_choice = input("원하는 검색 엔진의 번호를 입력하세요 (1 또는 2): ").strip()
+
+if engine_choice == '1':
+  search_engine = 'naver'
+elif engine_choice == '2':
+  search_engine = 'google'
+else:
+  print("잘못된 선택입니다. 기본값으로 네이버를 사용합니다.")
+  search_engine = 'naver'
+
 # 검색어 입력
 input_name = input('검색어 입력: ').strip().replace('/', '_').replace('\\', '_')
 current_time = datetime.now().strftime('%Y%m%d_%H%M%S')
 folder_name = f"{input_name}_{current_time}"
 
 try:
-  # 페이지 접근
-  driver.get(
-      f'https://search.naver.com/search.naver?where=image&sm=tab_jum&query={input_name}')
-  wait.until(EC.presence_of_all_elements_located(
-      (By.CSS_SELECTOR, '._fe_image_tab_content_thumbnail_image')))
+  if search_engine == 'naver':
+    # 네이버 이미지 검색 URL
+    search_url = f'https://search.naver.com/search.naver?where=image&sm=tab_jum&query={input_name}'
+    driver.get(search_url)
+    wait.until(EC.presence_of_all_elements_located(
+        (By.CSS_SELECTOR, '._fe_image_tab_content_thumbnail_image')))
 
-  # 스크롤 다운
-  scroll_pause_time = 3
-  last_height = driver.execute_script("return document.body.scrollHeight")
+    # 스크롤 다운
+    scroll_pause_time = 3
+    last_height = driver.execute_script("return document.body.scrollHeight")
 
-  while True:
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(scroll_pause_time)
-    new_height = driver.execute_script("return document.body.scrollHeight")
-    if new_height == last_height:
-      break
-    last_height = new_height
+    while True:
+      driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+      time.sleep(scroll_pause_time)
+      new_height = driver.execute_script("return document.body.scrollHeight")
+      if new_height == last_height:
+        break
+      last_height = new_height
 
-  # 이미지 목록 불러오기
-  imgs = driver.find_elements(By.CSS_SELECTOR,
-                              '._fe_image_tab_content_thumbnail_image')
+    # 이미지 목록 불러오기
+    imgs = driver.find_elements(By.CSS_SELECTOR,
+                                '._fe_image_tab_content_thumbnail_image')
+
+  elif search_engine == 'google':
+    # 구글 이미지 검색 URL
+    search_url = f'https://www.google.com/search?tbm=isch&q={input_name}'
+    driver.get(search_url)
+    wait.until(EC.presence_of_all_elements_located(
+        (By.CSS_SELECTOR, 'img.YQ4gaf')))
+
+    # 스크롤 다운 및 더 보기 클릭
+    scroll_pause_time = 2
+    last_height = driver.execute_script("return document.body.scrollHeight")
+
+    while True:
+      driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+      time.sleep(scroll_pause_time)
+      # "더 보기" 버튼 클릭 시도
+      try:
+        load_more = driver.find_element(By.CSS_SELECTOR, ".mye4qd")
+        driver.execute_script("arguments[0].click();", load_more)
+        time.sleep(scroll_pause_time)
+      except:
+        pass  # 더 이상 "더 보기" 버튼이 없을 경우 무시
+
+      new_height = driver.execute_script("return document.body.scrollHeight")
+      if new_height == last_height:
+        break
+      last_height = new_height
+
+    # 이미지 목록 불러오기
+    imgs = driver.find_elements(By.CSS_SELECTOR, 'img.Q4LuWd')
+
+  else:
+    raise ValueError("지원되지 않는 검색 엔진입니다.")
 
   # 폴더 생성
   createFolder(folder_name)
@@ -92,46 +144,94 @@ try:
   max_images = 100  # 다운로드할 최대 이미지 수 설정 (필요시 조정)
 
   while idx < total_images and idx < max_images:
-    imgs = driver.find_elements(By.CSS_SELECTOR,
-                                '._fe_image_tab_content_thumbnail_image')
-    if idx >= len(imgs):
-      print(f"총 {idx}개의 이미지를 처리했습니다.")
-      break
-    img = imgs[idx]
-    img_url = img.get_attribute('src')
+    if search_engine == 'naver':
+      imgs = driver.find_elements(By.CSS_SELECTOR,
+                                  '._fe_image_tab_content_thumbnail_image')
+      if idx >= len(imgs):
+        print(f"총 {idx}개의 이미지를 처리했습니다.")
+        break
+      img = imgs[idx]
+      img_url = img.get_attribute('src')
 
-    driver.execute_script("arguments[0].click();", img)
-    try:
-      link = wait.until(EC.presence_of_element_located(
-          (By.CSS_SELECTOR, '.info_area > a.info_title')))
-      original_url = link.get_attribute('href')
+      driver.execute_script("arguments[0].click();", img)
+      try:
+        link = wait.until(EC.presence_of_element_located(
+            (By.CSS_SELECTOR, '.info_area > a.info_title')))
+        original_url = link.get_attribute('href')
 
-      title_elem = wait.until(EC.presence_of_element_located(
-          (By.CSS_SELECTOR, 'a.info_title > strong.title')))
-      title = sanitize_filename(title_elem.text.strip())
+        title_elem = wait.until(EC.presence_of_element_located(
+            (By.CSS_SELECTOR, 'a.info_title > strong.title')))
+        title = sanitize_filename(title_elem.text.strip())
 
-      file_name = f"{idx + 1}_{title}.jpg"
-      file_path = os.path.join(folder_name, file_name)
+        file_name = f"{idx + 1}_{title}.jpg"
+        file_path = os.path.join(folder_name, file_name)
 
-      # 중복된 링크가 있는 경우 건너뛰기
-      if original_url in original_urls:
-        print(f"중복 URL 확인 : {original_url} / 이미지 이름 : {title}")
+        # 중복된 링크가 있는 경우 건너뛰기
+        if original_url in original_urls:
+          print(f"중복 URL 확인 : {original_url} / 이미지 이름 : {title}")
+          image_data.append(
+              [file_name, original_url, "중복으로 인해 저장되지 않음"])  # 중복된 이미지의 정보 저장
+          driver.back()
+          wait.until(EC.presence_of_all_elements_located(
+              (By.CSS_SELECTOR, '._fe_image_tab_content_thumbnail_image')))
+          idx += 1
+          continue
+
+        original_urls.add(original_url)  # 새로운 URL을 세트에 추가
+
+      except Exception as e:
+        print(f"Error for Image {idx + 1}: {str(e)}")
+        original_url = None
+        title = "Unknown"
         image_data.append(
-            [file_name, original_url, "중복으로 인해 저장되지 않음"])  # 중복된 이미지의 정보 저장
-        driver.back()
-        wait.until(EC.presence_of_all_elements_located(
-            (By.CSS_SELECTOR, '._fe_image_tab_content_thumbnail_image')))
+            [f"{idx + 1}_Unknown.jpg", None, "오류로 인해 저장되지 않음"])  # 오류 발생시 정보 저장
+
+    elif search_engine == 'google':
+      imgs = driver.find_elements(By.CSS_SELECTOR, 'img.Q4LuWd')
+      if idx >= len(imgs):
+        print(f"총 {idx}개의 이미지를 처리했습니다.")
+        break
+      img = imgs[idx]
+      img_url = img.get_attribute('src')
+
+      try:
+        driver.execute_script("arguments[0].click();", img)
+      except Exception as e:
+        print(f"이미지 클릭 오류 {idx + 1}: {str(e)}")
+        image_data.append(
+            [f"{idx + 1}_Unknown.jpg", None, "클릭 오류로 인해 저장되지 않음"])
         idx += 1
         continue
 
-      original_urls.add(original_url)  # 새로운 URL을 세트에 추가
+      try:
+        # 원본 이미지 URL 가져오기
+        original_url = wait.until(EC.presence_of_element_located(
+            (By.CSS_SELECTOR, 'a.eHAdSb'))).get_attribute('href')
 
-    except Exception as e:
-      print(f"Error for Image {idx + 1}: {str(e)}")
-      original_url = None
-      title = "Unknown"
-      image_data.append(
-          [f"{idx + 1}_Unknown.jpg", None, "오류로 인해 저장되지 않음"])  # 오류 발생시 정보 저장
+        title = sanitize_filename(input_name)
+
+        file_name = f"{idx + 1}_{title}.jpg"
+        file_path = os.path.join(folder_name, file_name)
+
+        # 중복된 링크가 있는 경우 건너뛰기
+        if original_url in original_urls:
+          print(f"중복 URL 확인 : {original_url} / 이미지 이름 : {title}")
+          image_data.append(
+              [file_name, original_url, "중복으로 인해 저장되지 않음"])  # 중복된 이미지의 정보 저장
+          driver.execute_script("window.history.go(-1)")
+          wait.until(EC.presence_of_all_elements_located(
+              (By.CSS_SELECTOR, 'img.Q4LuWd')))
+          idx += 1
+          continue
+
+        original_urls.add(original_url)  # 새로운 URL을 세트에 추가
+
+      except Exception as e:
+        print(f"Error for Image {idx + 1}: {str(e)}")
+        original_url = None
+        title = "Unknown"
+        image_data.append(
+            [f"{idx + 1}_Unknown.jpg", None, "오류로 인해 저장되지 않음"])  # 오류 발생시 정보 저장
 
     # 이미지 저장 로직 (중복되지 않은 경우에만 실행)
     if original_url:
@@ -146,11 +246,16 @@ try:
               for chunk in response.iter_content(1024):
                 file.write(chunk)
           else:
-            print(f"Failed to download image {idx + 1}: HTTP {response.status_code}")
+            print(
+              f"Failed to download image {idx + 1}: HTTP {response.status_code}")
             image_data.append([file_name, original_url, "이미지 다운로드 실패"])
-            driver.back()
+            if search_engine == 'naver':
+              driver.back()
+            elif search_engine == 'google':
+              driver.execute_script("window.history.go(-1)")
             wait.until(EC.presence_of_all_elements_located(
-                (By.CSS_SELECTOR, '._fe_image_tab_content_thumbnail_image')))
+                (By.CSS_SELECTOR,
+                 'img.Q4LuWd' if search_engine == 'google' else '._fe_image_tab_content_thumbnail_image')))
             idx += 1
             continue
 
@@ -161,9 +266,14 @@ try:
         print(f"Error saving Image {idx + 1}: {str(e)}")
         image_data.append([file_name, original_url, "저장 중 오류 발생"])
 
-    driver.back()
-    wait.until(EC.presence_of_all_elements_located(
-        (By.CSS_SELECTOR, '._fe_image_tab_content_thumbnail_image')))
+    if search_engine == 'naver':
+      driver.back()
+      wait.until(EC.presence_of_all_elements_located(
+          (By.CSS_SELECTOR, '._fe_image_tab_content_thumbnail_image')))
+    elif search_engine == 'google':
+      driver.execute_script("window.history.go(-1)")
+      wait.until(EC.presence_of_all_elements_located(
+          (By.CSS_SELECTOR, 'img.Q4LuWd')))
     idx += 1
 
   # 엑셀 데이터 한 번에 저장 (이미지 이름, 원본 URL, 이미지 URL 순)
